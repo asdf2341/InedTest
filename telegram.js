5ec22f988e8848cc1518bb25798dae75b7547beb
Your Name <you@example.com>
2026-01-08 16:31:38 +0500

    change little bit

--- telegram.js
+++ telegram.js
@@ -1,27 +1,30 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { exec } from 'child_process';
 import { sendMessage, sendPhoto } from './telegramUtils.js'; // если telegram.js в корне
-import 'dotenv/config';
+import express from 'express';
+import path from 'path';
 
 const token = process.env.TELEGRAM_TOKEN;
-const chatId = process.env.TELEGRAM_CHAT_ID;
 const webhookUrl = process.env.WEBHOOK_URL;
-const bot = new TelegramBot(token);
+
+const bot = new TelegramBot(token, { polling: false });
 
 const app = express();
 app.use(express.json());
-// Регистрируем webhook
-bot.setWebHook(`${webhookUrl}/bot${token}`);
 
 app.post(`/bot${token}`, (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
-
+const playwrightBin = path.resolve('node_modules', '.bin', 'playwright');
 bot.onText(/\/run_test/, async () => {
   await sendMessage('🚀 Запускаю автотест...');
 
-  exec('npx playwright test InedTest/Locators.spec.js --timeout=120000', (error, stdout, stderr) => {
+  exec('${playwrightBin} test InedTest/Locators.spec.js --timeout=120000',  { maxBuffer: 1024 * 1024 * 5 }, (error, stdout, stderr) => {
+    console.log('===== STDOUT =====');
+    console.log(stdout);
+    console.log('===== STDERR =====');
+    console.log(stderr);
     if (error) {
       console.error('❌ Playwright test failed:', error);
       sendMessage(`❌ Ошибка при запуске теста:\n${error.message}`);
@@ -34,7 +37,6 @@ bot.onText(/\/run_test/, async () => {
     }
     sendMessage(`✅ Автотест завершился. Вывод:\n${stdout}`);
   });
-});
-
+}); 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
